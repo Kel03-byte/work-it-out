2d07f5c8608286fd7a430f49cfc8d2c1b24d67a3
Kellie Ryan <littlekel03@gmail.com>
2021-07-09 16:18:48 +0800

    Fixed up some API routing

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,47 @@
 const router = require('express').Router();
+const { request } = require('express');
 const Workout = require('../models/workout.js');
+
+// Find all Workouts
+router.get('/api/workouts', (request, response) => {
+    try {
+        Workout.find({})
+            .then(data => response.json(data))
+    } catch (error) {
+        console.log(error.message)
+    };
+})
+
+
+// Find Workouts by Range
+router.get('/api/workouts/range', (request, response) => {
+    try {
+        Workout.find({})
+            .then(data => response.json(data))
+    } catch (error) {
+        console.log(error.message)
+    };
+})
+
+// Update a Workout using Id
+router.put('/api/workouts/:id', (request, response) => {
+    try {
+        Workout.findByIdAndUpdate(request.params.id, request.body)
+            .then(data => response.json(data))
+    } catch (error) {
+        response.status(500).json(error.message);
+    }
+})
+
+// Create a new Workout
+router.post('/api/workouts', ({ body }, response) => {
+    Workout.create(body)
+        .then((workoutData) => {
+            response.json(workoutData);
+        })
+        .catch((error) => {
+            response.status(500).json(error.message);
+        });
+});
 
 module.exports = router;
